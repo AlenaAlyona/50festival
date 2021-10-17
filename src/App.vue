c3effa006d9582ae53f3eb273edf8346f0088264
AlenaAlyona <izakson91@gmail.com>
2021-10-17 17:11:27 +0200

    fix: App.vue after adding typescript

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <FiftyFestival />
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent } from "vue";
+import FiftyFestival from "./components/FiftyFestival.vue";
 
 export default defineComponent({
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
+    FiftyFestival,
+  },
 });
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: "LunchType-Regular", sans-serif;
+  font-size: 18px;
+  line-height: 1.5;
 }
 </style>
